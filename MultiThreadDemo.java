9b2253e46c9673a054863b826b5ed56bbd598b37
Abhisek <mandalabhisek20@gmail.com>
2025-04-18 09:13:15 +0530

    MultiThreading added

---
+++ MultiThreadDemo.java
@@ -0,0 +1,46 @@
+class ThreadX extends Thread{
+	public void run() {
+		
+	    	 try {
+	    		 Thread t= Thread.currentThread();
+	    	     t.setName("ThreadX");
+	    	     for(int i=1;i<=10;i++) {
+	    	    	 System.out.println(t.getName()+":"+ i);
+				Thread.sleep(1000);
+	    	     }
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+	     }
+	}
+	
+class ThreadY extends Thread{
+	public void run() {
+		
+			try {
+				Thread t =Thread.currentThread();
+				t.setName("ThreadY");
+				for(int i=10;i>=1;i--) {
+				System.out.println(t.getName()+":"+i);
+				Thread.sleep(1500);
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+public class MultiThreadDemo {
+
+	public static void main(String[] args) {
+		ThreadX t1= new ThreadX();
+		t1.start();
+		ThreadY t2= new ThreadY();
+		t2.start();
+			
+
+	}
+
+}
